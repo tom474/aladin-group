3c5b3cf98b13c5fad859117d165df3f823632753
Terry275 <116662781+Terry275@users.noreply.github.com>
2023-05-10 23:09:45 +0700

    add media queries

--- static-website-material/assets/js/index.js
+++ static-website-material/assets/js/index.js
@@ -1,4 +1,4 @@
-// the show/hide pssword button
+// the show/hide password button
 
 const togglePassword = document.querySelector('#togglePassword');
 const password = document.querySelector('#password');
@@ -19,6 +19,7 @@ togglePassword.addEventListener('click', function (e) {
 });
 
 
+
 // set profile picture
 
 const imgDiv = document.querySelector('.profile_pic');
@@ -26,35 +27,30 @@ const img = document.querySelector('#photo');
 const file = document.querySelector('#file');
 const uploadBtn = document.querySelector('#upload_button');
 
-// when user hover, display
-
+// when user hover over the image, display the upload button
 imgDiv.addEventListener('mouseenter', function(){
     uploadBtn.style.display = "block";
 });
 
-// when they hover out
-
+// when they hover out, hide the upload button
 imgDiv.addEventListener('mouseleave', function(){
     uploadBtn.style.display = "none";
 });
 
 // choose an image to upload
-
 file.addEventListener('change', function(){
     // this refers to the file
     const choosedFile = this.files[0];
 
     if (choosedFile) {
-
         const reader = new FileReader(); 
-        // FileReader is a predefined function of JS
 
+        // FileReader is a predefined function of JS
         reader.addEventListener('load', function(){
             img.setAttribute('src', reader.result);
         });
 
         reader.readAsDataURL(choosedFile);
-
     }
 });
 
